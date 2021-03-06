8e188dc882e10fd76ad94f3cfe994b9744b9c961
raintygao <726848199@qq.com>
2021-03-06 13:47:40 +0800

    feat: add terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pluginJSON from '@rollup/plugin-json'
+import { terser } from 'rollup-plugin-terser';
 
 export default {
 		input: 'src/index.js',
 		output: { file: 'dist/bundle.js', format: 'cjs' },
 		plugins: [
 			pluginJSON(),
-			resolve(), // so Rollup can find `ms`
-			commonjs() // so Rollup can convert `ms` to an ES module
+			resolve(), 
+			commonjs(),
+			terser()
 		]
 }
